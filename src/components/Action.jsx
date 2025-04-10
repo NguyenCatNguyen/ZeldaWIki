06665d6e43d416242b0b0a26b696d1c9dccc4649
NguyenCatNguyen <catngn11@gmail.com>
2025-04-10 01:01:40 -0500

    update

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -4,16 +4,22 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Action = () => {
   return (
-    <div className=' w-full flex flex-col sm:flex-row justify-around items-center px-15'>
-        <div className='bg-blue-500 w-full mx-10 rounded flex justify-between items-center p-10 pt-40'>
-            <p className=' w-2/4 text-5xl'>Like what you see, let get connect!</p>
-            <a className='bg-white text-blue-500 h-12 w-18 flex justify-center items-center rounded' href='https://www.linkedin.com/in/cat-nguyen-626621235/' target='_blank' rel='noopener noreferrer'>
-                <FaExternalLinkAlt  className='size-6'/>
-            </a>
-
-        </div>
+    <div className='w-full flex flex-col sm:flex-row justify-around items-center sm:px-15 px-8'>
+      <div className='bg-blue-500 w-full rounded sm:justify-between justify-center items-center p-10 pt-10 sm:pt-30 md:pt-40 block sm:flex text-center'>
+        <p className='sm:w-2/4 text-2xl sm:text-5xl text-center sm:text-left'>Like what you see, let’s get connected!</p>
+        <a
+          className=' hover:bg-blue-950 hover:text-white hover:w-22 transition-color duration-500 ease-in-out
+                    bg-white text-blue-500 h-12 w-18 flex justify-center items-center rounded mx-auto sm:mx-0 mt-5 sm:mt-0'
+          href='https://www.linkedin.com/in/cat-nguyen-626621235/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaExternalLinkAlt className='size-6' />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Action
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,15 +12,18 @@ const navItems = [
 
 const Footer = () => {
   return (
-    <div className=' w-full flex flex-col sm:flex-row justify-around items-center py-5'>
-      <div className='text-gold font-rye text-3xl'>CN</div>
-      <div className='py-5'>
+    <div className=' w-full px-15 text-white/80 flex flex-col  mt-15 bg-slate-900 py-10'>
+      <div className='flex flex-col sm:flex-row justify-around sm:items-center sm:px-15 px-0'>
         {navItems.map((item, i) => (
-          <a className='px-3 cursor-pointer'
-           href={item.href} key={i}>{item.label}</a>
+          <a className='cursor-pointer text-2xl border-b-1 pt-5 pl-2 
+                      hover:text-white duration-400 transition-color 
+                       sm:border-none sm:pl-0' href={item.href} key={i}>{item.label}</a>
         ))}
       </div>
-      <div className='text-center'>Coppyright @ 2025 NguyenCatNguyen <br />Developed by NguyenCat Nguyen</div>
+      <div>
+        <div className='text-gold font-rye text-3xl text-center py-5 pt-10'>CN</div>
+        <div className='text-center'>Coppyright @ 2025 NguyenCatNguyen <br />Developed by NguyenCat Nguyen</div>
+      </div>
       
     </div>
   )
