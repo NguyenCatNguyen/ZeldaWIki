715d87686e84b0b19ed34013d25ec6c8b538287b
NguyenCatNguyen <catngn11@gmail.com>
2025-04-12 01:04:36 -0500

    Create page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production'
-    ? '/ZeldaWIki/'  // Match your GitHub repo name exactly
-    : '/',           // Root path for dev: '/ZeldaWIki',
+  base: '/ZeldaWIki/',       
 })
